ee781a146cd2c9322bd05a1bf4fd109269d583b0
Juanjo Guerrero <jcerezuela@veinteractive.com>
2015-10-10 17:47:13 +0100

    Stablishing Code Structure

---
+++ TDDKatas/XUnit/FizzBuzz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TDDKatas.FizzBuzz.Nunit
+{
+    
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void DoSomething_MustDoSomething()
+        {
+
+        }
+    }
+}
