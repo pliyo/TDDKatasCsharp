9a35bd5930f0233ec06a98b9f78fb6c7504abbdc
Juanjo Guerrero <jcerezuela@veinteractive.com>
2015-10-14 23:42:06 +0100

    Bowling Kata First Test

---
+++ BowlingKata/Bowling.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKata
+{
+    public class Bowling
+    {
+        public int KnockedDown;
+
+        public int Roll(int pins)
+        {
+            KnockedDown = pins;
+            return KnockedDown;
+        }
+    }
+}
---
+++ BowlingKata/BowlingTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace BowlingKata
+{
+    public class BowlingTests
+    {
+
+        [Fact]
+        [Theory]
+        [InlineData(1)]
+        public void Roll_Set_The_Number_Of_Pins_Knocked_Down(int pins)
+        {
+            Bowling bowling = new Bowling();
+            int result = bowling.Roll(pins);
+            Assert.Equal(result, pins);
+        }
+    }
+}
